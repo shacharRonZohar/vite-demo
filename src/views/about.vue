b1c6088890d63196c20fc68754aa5c64fc57ed8f
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2022-07-11 10:39:54 +0300

    Finished cleanup

---
+++ src/views/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>This is an about page</h2>
+</template>
+
+<script>
+
+export default {
+  name: 'about'
+}
+</script>
+
+<style>
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>This is a home page</h2>
+</template>
+
+<script>
+
+export default {
+  name: 'home'
+}
+</script>
+
+<style>
+</style>
